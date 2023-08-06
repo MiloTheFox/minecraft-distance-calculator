fb5d2d5bedb0e832be5c86a0889df10ddd6e910a
Sweet-Tooth11 <80362356+Sweet-Tooth11@users.noreply.github.com>
2023-08-06 15:54:04 +0200

    Delete compile.bat

--- compile.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-javac --module-path "./libs/javafx-sdk-17.0.7/lib/javafx.fxml.jar;./libs/javafx-sdk-17.0.7/lib/javafx-swt.jar;./libs/javafx-sdk-17.0.7/lib/javafx.graphics.jar;./libs/javafx-sdk-17.0.7/lib/javafx.swing.jar;./libs/javafx-sdk-17.0.7/lib/javafx.web.jar;./libs/javafx-sdk-17.0.7/lib/javafx.base.jar;./libs/javafx-sdk-17.0.7/lib/javafx.controls.jar" --add-modules javafx.controls,javafx.fxml DistanceCalculatorGUI.java
-@echo DistanceCalculatorGUI.java has successfully been compiled
