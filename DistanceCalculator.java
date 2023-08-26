49b88480536698a63582655d0d7f5a30e86c26cb
Sweet-Tooth11 <80362356+Sweet-Tooth11@users.noreply.github.com>
2023-08-27 00:41:29 +0200

    Update DistanceCalculator.java

--- DistanceCalculator.java
+++ DistanceCalculator.java
@@ -1,43 +1,45 @@
 /**
  * @author Sweet_Tooth11
- * @version v1.3, 08/20/2023 - 04:51 PM GMT+1
+ * @version v1.3, 08/27/2023 - 12:41AM GMT+1
  */
 
-import java.awt.Color;
-import java.awt.GridLayout;
+// Import statements...
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
-
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-import javax.swing.*;
-
 public class Main {
-    private static final BigDecimal TWO = BigDecimal.valueOf(2);
-    private static final int SCALE = 10;
+    // Defining the Constant...
+    private static final int NUM_COORDINATES = 6;
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(Main::createAndShowGUI);
     }
 
     private static void createAndShowGUI() {
+        // Set up the JFrame and UI components...
         JFrame frame = new JFrame("Distance Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new GridLayout(9, 2));
+        frame.setLayout(new GridLayout(NUM_COORDINATES + 1, 2));
         frame.setBackground(Color.BLACK);
 
-        JTextField[] textFields = new JTextField[6];
-        for (int i = 0; i < 6; i++) {
+        JTextField[] textFields = new JTextField[NUM_COORDINATES];
+        JLabel[] coordinateLabels = new JLabel[NUM_COORDINATES];
+
+        for (int i = 0; i < NUM_COORDINATES; i++) {
             textFields[i] = new JTextField(5);
-            frame.add(createCoordinateLabel(i));
+            coordinateLabels[i] = createCoordinateLabel(i);
+            frame.add(coordinateLabels[i]);
             frame.add(textFields[i]);
         }
 
         JButton button1 = new JButton("Euclidean Distance");
         JButton button2 = new JButton("Manhattan Distance");
 
-        button1.addActionListener(e -> calculateDistanceAndShowResult(e, textFields, "Euclidean"));
-        button2.addActionListener(e -> calculateDistanceAndShowResult(e, textFields, "Manhattan"));
+        button1.addActionListener(e -> calculateAndShowResult(textFields, DistanceType.EUCLIDEAN));
+        button2.addActionListener(e -> calculateAndShowResult(textFields, DistanceType.MANHATTAN));
 
         frame.add(button1);
         frame.add(button2);
@@ -48,84 +50,104 @@ public class Main {
     }
 
     private static JLabel createCoordinateLabel(int i) {
-        String label = i % 3 == 0 ? "x" + (i / 3 + 1) + ":"
-                : i % 3 == 1 ? "y" + (i / 3 + 1) + ":" : "z" + (i / 3 + 1) + ":";
+        // Create a label for a coordinate input field...
+        String label = String.format("Coordinate %d:", i + 1);
         return new JLabel(label);
     }
 
-    private static void calculateDistanceAndShowResult(ActionEvent event, JTextField[] textFields,
-            String distanceType) {
-        try {
-            BigDecimal[] coordinates = new BigDecimal[6];
-            for (int i = 0; i < 6; i++) {
-                String input = textFields[i].getText();
-                if (!isValidNumber(input)) {
-                    showErrorDialog("Please enter valid numbers for all coordinates.");
-                    return;
-                }
-                coordinates[i] = new BigDecimal(input);
-            }
+    private static BigDecimal[] extractCoordinates(JTextField[] textFields) {
+        // Extract and parse coordinates from text fields...
+        BigDecimal[] coordinates = new BigDecimal[NUM_COORDINATES];
+        for (int i = 0; i < NUM_COORDINATES; i++) {
+            String input = textFields[i].getText();
+            coordinates[i] = new BigDecimal(input);
+        }
+        return coordinates;
+    }
 
-            BigDecimal result = BigDecimal.ZERO;
-            if (distanceType.equals("Euclidean")) {
-                result = calculateEuclideanDistance(coordinates[0], coordinates[1], coordinates[2],
-                        coordinates[3], coordinates[4], coordinates[5]);
-            } else if (distanceType.equals("Manhattan")) {
-                result = calculateManhattanDistance(coordinates[0], coordinates[1], coordinates[2],
-                        coordinates[3], coordinates[4], coordinates[5]);
-            }
+    private static void calculateAndShowResult(JTextField[] textFields, DistanceType distanceType) {
+            // Extract coordinates from text fields and calculate distance...
+        try {
+            BigDecimal[] coordinates = extractCoordinates(textFields);
+            BigDecimal result = calculateDistance(coordinates, distanceType);
             showInfoDialog("The result is: " + result);
+            // Return an error if any field is neither a number nor holds a value...
         } catch (NumberFormatException ex) {
             showErrorDialog("Please enter valid numbers for all coordinates.");
         }
     }
 
-    private static boolean isValidNumber(String input) {
-        try {
-            new BigDecimal(input);
-            return true;
-        } catch (NumberFormatException | NullPointerException Exception) {
-            return false;
+    private static BigDecimal calculateDistance(BigDecimal[] coordinates, DistanceType distanceType) {
+        // Calculate distance based on distance type...
+        int xStartIndex = Indices.X_START.getIndex();
+        int xEndIndex = Indices.X_END.getIndex();
+        int yStartIndex = Indices.Y_START.getIndex();
+        int yEndIndex = Indices.Y_END.getIndex();
+        int zStartIndex = Indices.Z_START.getIndex();
+        int zEndIndex = Indices.Z_END.getIndex();
+
+        BigDecimal xDifference = coordinates[xEndIndex].subtract(coordinates[xStartIndex]);
+        BigDecimal yDifference = coordinates[yEndIndex].subtract(coordinates[yStartIndex]);
+        BigDecimal zDifference = coordinates[zEndIndex].subtract(coordinates[zStartIndex]);
+
+        switch (distanceType) {
+            case EUCLIDEAN:
+                BigDecimal sumOfSquares = xDifference.pow(2).add(yDifference.pow(2)).add(zDifference.pow(2));
+                double sqrtValue = Math.sqrt(sumOfSquares.doubleValue());
+                return round(BigDecimal.valueOf(sqrtValue), 0);
+            case MANHATTAN:
+                BigDecimal xAbs = xDifference.abs();
+                BigDecimal yAbs = yDifference.abs();
+                BigDecimal zAbs = zDifference.abs();
+                return xAbs.add(yAbs).add(zAbs);
+            default:
+                throw new IllegalArgumentException("Unknown distance type");
         }
-    }
-
-    private static void showErrorDialog(String message) {
-        JOptionPane.showMessageDialog(null, message, "Error", JOptionPane.ERROR_MESSAGE);
-    }
-
-    private static void showInfoDialog(String message) {
-        JOptionPane.showMessageDialog(null, message, "Result", JOptionPane.INFORMATION_MESSAGE);
-    }
-
-    private static BigDecimal calculateEuclideanDistance(BigDecimal x1, BigDecimal y1, BigDecimal z1,
-            BigDecimal x2, BigDecimal y2, BigDecimal z2) {
-        BigDecimal dx = x2.subtract(x1);
-        BigDecimal dy = y2.subtract(y1);
-        BigDecimal dz = z2.subtract(z1);
-
-        return round(sqrt(dx.pow(2).add(dy.pow(2)).add(dz.pow(2))), 0);
-    }
-
-    private static BigDecimal calculateManhattanDistance(BigDecimal x1, BigDecimal y1, BigDecimal z1,
-            BigDecimal x2, BigDecimal y2, BigDecimal z2) {
-        BigDecimal dx = x2.subtract(x1).abs();
-        BigDecimal dy = y2.subtract(y1).abs();
-        BigDecimal dz = z2.subtract(z1).abs();
-
-        return dx.add(dy).add(dz);
     }
 
     private static BigDecimal round(BigDecimal value, int scale) {
+        // Custom function to round a BigDecimal value...
         return value.setScale(scale, RoundingMode.HALF_UP);
     }
 
-    private static BigDecimal sqrt(BigDecimal x) {
-        BigDecimal approx = BigDecimal.ONE;
-        BigDecimal sqrt = x.divide(TWO, SCALE, RoundingMode.DOWN);
-        while (!approx.equals(sqrt)) {
-            approx = sqrt;
-            sqrt = x.divide(approx, SCALE, RoundingMode.HALF_UP).add(approx).divide(TWO, SCALE, RoundingMode.HALF_UP);
+    private static void showErrorDialog(String message) {
+	// Show an error dialog
+        showDialog(message, "Error", JOptionPane.ERROR_MESSAGE);
+    }
+
+    private static void showInfoDialog(String message) {
+        // Show an info dialog...
+        showDialog(message, "Result", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private static void showDialog(String message, String title, int messageType) {
+        // Show a generic dialog...
+        JOptionPane.showMessageDialog(null, message, title, messageType);
+    }
+
+    private enum Indices {
+        // Enum for indices of coordinates...
+        X_START(0),
+        Y_START(1),
+        Z_START(2),
+        X_END(3),
+        Y_END(4),
+        Z_END(5);
+
+        private final int index;
+
+        Indices(int index) {
+            this.index = index;
         }
-        return sqrt;
+
+        public int getIndex() {
+            return index;
+        }
+    }
+
+    private enum DistanceType {
+        // Enum for the available distance types...
+        EUCLIDEAN,
+        MANHATTAN
     }
 }
